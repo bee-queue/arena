83d9ee55b01f2a47dbe2e5ccee53194b02135ca9
Randall Ma <randall@randallma.com>
2017-05-25 12:18:26 -0700

    add high level queue stats

--- src/server/views/dashboard/queueDetails.js
+++ src/server/views/dashboard/queueDetails.js
@@ -1,4 +1,14 @@
+const _ = require('lodash');
 const Queues = require('../../bull');
+
+const metrics = [
+  'redis_version',
+  'total_system_memory',
+  'used_memory',
+  'mem_fragmentation_ratio',
+  'connected_clients',
+  'blocked_clients'
+];
 
 async function handler(req, res) {
   const name = req.params.queueName;
@@ -11,26 +21,45 @@ async function handler(req, res) {
       });
   }
 
-  const promises = [
-    queue.getActiveCount(),
-    queue.getFailedCount(),
-    queue.getDelayedCount(),
-    queue.getWaitingCount(),
-    queue.getPausedCount()
-  ];
+  /*
+    TODO(randall): get feedback on practicality of this idea
+    Description: Get elapsed time for currently processing job
 
-  const [ activeCount, failedCount, delayedCount, waitingCount, pausedCount ] = await Promise.all(promises);
+    const jobTypes = ['waiting', 'active', 'completed', 'failed', 'delayed'];
+    const jobTimestamps = (await Promise.all(jobTypes.map((type) => queue[`get${_.capitalize(type)}`](0, 0))))
+      .map((jobs) => _.first(jobs) || {})
+      .map((job) => job['timestamp']);
+    const jobTimestampsMap = {};
+    for (let typeIndex of _.range(jobTypes.length)) {
+      jobTimestampsMap[jobTypes[typeIndex]] = jobTimestamps[typeIndex];
+    }
+  */
+
+  const getStats = queue.client
+    .info()
+    .then((doc) => {
+      return _mapToObject(new Map(
+        doc.split('\r\n')
+          .map((line) => line.split(':'))
+          .filter((line) => _.includes(metrics, line[0]))
+      ));
+    });
+
+  const [jobCounts, stats] = await Promise.all([queue.getJobCounts(), getStats]);
 
   res.render('dashboard/templates/queueDetails.hbs', {
     name,
-    groups: [
-      ['active', activeCount],
-      ['failed', failedCount],
-      ['delayed', delayedCount],
-      ['waiting', waitingCount],
-      ['paused', pausedCount]
-    ]
+    jobCounts,
+    stats
   });
+}
+
+function _mapToObject(map) {
+  let obj = {};
+  for (let [k, v] of map) {
+    obj[k] = v;
+  }
+  return obj;
 }
 
 module.exports = handler;
