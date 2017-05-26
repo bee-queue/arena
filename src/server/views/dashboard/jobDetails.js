eb34af1ddd9c95471e5e3bd3bfcae2a5ed26e551
Randall Ma <randall@randallma.com>
2017-05-26 11:24:43 -0700

    add job 404 page

--- src/server/views/dashboard/jobDetails.js
+++ src/server/views/dashboard/jobDetails.js
@@ -5,11 +5,12 @@ async function handler(req, res) {
   const { queueName, id } = req.params;
   const { json } = req.query;
   const jobTypes = ['waiting', 'active', 'completed', 'failed', 'delayed'];
+
   const queue = await Queues.get(queueName);
   if (!queue) res.status(404).render('dashboard/templates/queueNotFound.hbs', {name: queueName});
-  // if (!_.includes(jobTypes, state)) res.status(400).render('dashboard/templates/jobStateNotFound.hbs', {name: queueName, state});
 
   const job = await queue.getJob(id);
+  if (!job) res.status(404).render('dashboard/templates/jobNotFound.hbs', {id});
 
   if (json === 'true') {
     return res.json(job);
