1b3f6fe3e8eac73e7d701d870a565c2016ddb875
Randall Ma <randall@randallma.com>
2017-05-26 14:19:28 -0700

    add ability to delete completed tasks

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,17 +1,45 @@
 $(document).ready(() => {
+  function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  function getField(form, fieldName) {
+    return $(form).serializeArray().find((field) => field.name === fieldName).value;
+  }
+
   $('.js-retryjob-form').on('submit', function(e) {
     e.preventDefault();
 
-    const jobId = $(this).serializeArray().find((field) => field.name === 'jobId').value;
-    const queueName = $(this).serializeArray().find((field) => field.name === 'queueName').value;
+    const jobId = getField(this, 'jobId')
+    const queueName = getField(this, 'queueName');
 
     const r = window.confirm(`Retry job #${jobId} in queue "${queueName}"?`);
     if (r) {
       $.ajax({
         method: 'PATCH',
-        url: `/dashboard/${queueName}/${jobId}/retry`
+        url: `/dashboard/${queueName}/${jobId}`
       }).done(() => {
         window.location.reload();
+      }).fail((jqXHR, textStatus) => {
+        window.alert(`Request failed: ${textStatus}`);
+      });
+    }
+  });
+
+  $('.js-deletejob-form').on('submit', function(e) {
+    e.preventDefault();
+
+    const jobId = getField(this, 'jobId')
+    const queueName = getField(this, 'queueName');
+    const jobState = getField(this, 'jobState');
+
+    const r = window.confirm(`Retry job #${jobId} in queue "${queueName}"?`);
+    if (r) {
+      $.ajax({
+        method: 'DELETE',
+        url: `/dashboard/${queueName}/${jobId}`
+      }).done(() => {
+        window.location.href = `/dashboard/${queueName}/${jobState}`;
       }).fail((jqXHR, textStatus) => {
         window.alert(`Request failed: ${textStatus}`);
       });
--- src/server/views/dashboard/index.js
+++ src/server/views/dashboard/index.js
@@ -5,11 +5,13 @@ const queueDetails = require('./queueDetails');
 const queueJobsByState = require('./queueJobsByState');
 const jobDetails = require('./jobDetails');
 const jobRetry = require('./jobRetry');
+const jobDelete = require('./jobDelete');
 
 router.get('/', queueList);
 router.get('/:queueName', queueDetails);
 router.get('/:queueName/:id(\\d+)', jobDetails);
-router.patch('/:queueName/:id(\\d+)/retry', jobRetry);
+router.patch('/:queueName/:id(\\d+)', jobRetry);
+router.delete('/:queueName/:id(\\d+)', jobDelete);
 router.get('/:queueName/:state', queueJobsByState);
 
 module.exports = router;
