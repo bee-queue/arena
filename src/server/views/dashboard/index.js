12a84e524c186949881e577e9aafedd51bedb8d8
Randall Ma <randall@randallma.com>
2017-06-06 11:39:44 -0700

    fix permalinks

--- src/server/views/dashboard/index.js
+++ src/server/views/dashboard/index.js
@@ -7,7 +7,7 @@ const jobDetails = require('./jobDetails');
 
 router.get('/', queueList);
 router.get('/:queueName', queueDetails);
-router.get('/:queueName/:state', queueJobsByState);
 router.get('/:queueName/:id(\\d+)', jobDetails);
+router.get('/:queueName/:state', queueJobsByState);
 
 module.exports = router;
