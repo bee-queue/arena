e5c5fbb0f965f52397377088110f2be42b497947
Alex Miles <alex@studiomile.com>
2018-07-19 21:59:25 -0400

    Add semis back

--- src/server/views/routes.js
+++ src/server/views/routes.js
@@ -1,13 +1,13 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
-const dashboardRoutes = require('./dashboard')
-const apiRoutes = require('./api')
+const dashboardRoutes = require('./dashboard');
+const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes)
-router.use('/', dashboardRoutes)
+router.use('/api', apiRoutes);
+router.use('/', dashboardRoutes);
 
 router.get('/healthz', (req, res) => {
-  res.status(200).send(':)')
+  res.status(200).send(':)');
 })
 
 module.exports = router
