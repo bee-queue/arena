6da2f28abb561099c167c8a503c1f1c28e37c281
Alex Nesterov <alex@nesterov.email>
2019-06-06 22:27:29 +0300

    Add buttons with stats at queue overview

--- src/server/views/dashboard/queueList.js
+++ src/server/views/dashboard/queueList.js
@@ -1,9 +1,14 @@
-function handler(req, res) {
+async function handler(req, res) {
   const {Queues} = req.app.locals;
   const queues = Queues.list();
   const basePath = req.baseUrl;
-
-  return res.render('dashboard/templates/queueList', { basePath, queues });
+  let queuesList = [];
+  for (const queueList of queues) {
+    const queue = await Queues.get(queueList.name, queueList.hostId);
+    const stats = await queue.getJobCounts();
+    queuesList.push(Object.assign({}, queueList, {stats}));
+  }
+  return res.render('dashboard/templates/queueList', {basePath, queues: queuesList});
 }
 
 module.exports = handler;
