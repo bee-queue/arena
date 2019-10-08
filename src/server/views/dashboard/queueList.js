855cc35fb93a5a944a9640a0c93302c20d77237b
Francisco Sobrinho <francicsobrinho@gmail.com>
2019-10-08 10:41:30 -0300

    added resume to queueList

--- src/server/views/dashboard/queueList.js
+++ src/server/views/dashboard/queueList.js
@@ -1,8 +1,12 @@
-function handler(req, res) {
+async function handler(req, res) {
   const {Queues} = req.app.locals;
   const queues = Queues.list();
   const basePath = req.baseUrl;
 
+  for (let queue of queues){
+    queue.jobs = await Queues.get(queue.name, queue.hostId).then(queue=>queue.getJobCounts())
+  }
+  
   return res.render('dashboard/templates/queueList', { basePath, queues });
 }
 
