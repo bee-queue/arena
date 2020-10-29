d1d42737d9bad682c61f1a3d71897c58236131c7
Rafael <raprp@posteo.de>
2020-10-29 21:09:48 +0000

    fixed job listing for Bull

--- src/server/views/dashboard/queueJobsByState.js
+++ src/server/views/dashboard/queueJobsByState.js
@@ -101,13 +101,11 @@ async function _html(req, res) {
     jobs = jobs.filter((job) => job);
   } else {
     jobs = await queue[`get${_.capitalize(state)}`](startId, endId);
-    await jobs.map(
-      Promise.all(async (job) => {
-        let logs = await queue.getJobLogs(job.id);
-        job.logs = logs.logs || 'No Logs';
-        return job;
-      })
-    );
+    await Promise.all(jobs.map(async job => {
+      let logs = await queue.getJobLogs(job.id);
+      job.logs = logs.logs || 'No Logs';
+      return job;
+    }));
   }
 
   for (const job of jobs) {
