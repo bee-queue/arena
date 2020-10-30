ac5b1790fa062c56d378ec232f4cd4d35175fa4d
Eli Skeggs <skeggse@users.noreply.github.com>
2020-10-30 00:53:21 -0700

    ci: include updated changelog in release

--- release.config.js
+++ release.config.js
@@ -3,9 +3,9 @@ module.exports = {
     ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
     ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits' }],
     '@semantic-release/github',
-    '@semantic-release/npm',
     '@semantic-release/changelog',
     ['@semantic-release/exec', { prepareCmd: 'npx prettier --write CHANGELOG.md' }],
+    '@semantic-release/npm',
     '@semantic-release/git',
   ],
 };
