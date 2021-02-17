0fa5d02ec03a53909de5519b0a2aa8f5f38065de
rogger andré valverde flores <rogger.valverde@uni.pe>
2021-02-16 23:10:33 -0500

    fix(dashboard): refresh page when adding a new job

--- public/dashboard.js
+++ public/dashboard.js
@@ -176,6 +176,7 @@ $(document).ready(() => {
       .done(() => {
         alert('Job successfully added!');
         localStorage.removeItem('arena:savedJob');
+        window.location.reload();
       })
       .fail((jqXHR) => {
         window.alert('Failed to save job, check console for error.');
