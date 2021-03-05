7c0bc7c8697d20513ebf8314295dd866f61112e7
Brad Vogel <brad@mixmax.com>
2021-03-05 17:10:23 -0500

    feat: better example showing jobs move through states

--- example/bee.js
+++ example/bee.js
@@ -12,6 +12,23 @@ async function main() {
   const server = new RedisServer(REDIS_SERVER_PORT);
   await server.open();
 
+  const queue = new Bee('name_of_my_queue', {
+    redis: {
+      port: REDIS_SERVER_PORT,
+    },
+  });
+
+  // Fake process function to move newly created jobs in the UI through a few of the job states.
+  queue.process(async function (job) {
+    // Wait 5sec
+    await new Promise((res) => setTimeout(res, 5000));
+
+    // Randomly succeeds or fails the job to put some jobs in completed and some in failed.
+    if (Math.random() > 0.5) {
+      throw new Error('fake error');
+    }
+  });
+
   Arena(
     {
       Bee,
