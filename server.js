53f703f65fb42b856fe45b663bdfd6a3dfa1b735
asamad <asamad@digitalocean.com>
2024-03-25 23:39:23 +0500

    added docker compose file and added more queues n the system

--- server.js
+++ server.js
@@ -14,6 +14,26 @@ Run(
               "host": process.env.REDIS_URL,
               "tls": {}
             }
+          },
+          {
+            "type": "bull",
+            "name": "MixPanelQueue",
+            "hostId": "CW",
+            "redis": {
+              "port": 6379,
+              "host": process.env.REDIS_URL,
+              "tls": {}
+            }
+          },
+          {
+            "type": "bull",
+            "name": "CustomerIOEvent",
+            "hostId": "CW",
+            "redis": {
+              "port": 6379,
+              "host": process.env.REDIS_URL,
+              "tls": {}
+            }
           }
         ],
       }
