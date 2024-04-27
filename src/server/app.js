2fcbe0f4395d4b6ff911907542725fe503889b0b
roggervalf <rogger.valverde@uni.pe>
2024-04-27 13:22:25 -0500

    chore: update version

--- src/server/app.js
+++ src/server/app.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
   app.locals.vendorPath = '/vendor';
   app.locals.customCssPath = config.customCssPath;
   app.locals.customJsPath = config.customJsPath;
-  app.locals.version = '4.2.0';
+  app.locals.version = '4.3.0';
 
   app.set('views', `${__dirname}/views`);
   app.set('view engine', 'hbs');
