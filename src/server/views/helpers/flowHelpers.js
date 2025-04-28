46fed01f006c35db60a786c1be1d71e53e0ff4e2
roggervalf <rogger.valvrd@gmail.com>
2025-04-27 20:37:40 -0600

    fix(get-flow): consider when there are not children

--- src/server/views/helpers/flowHelpers.js
+++ src/server/views/helpers/flowHelpers.js
@@ -1,11 +1,11 @@
 const Helpers = {
   processFlow: function (flow) {
     if (!flow) return {};
-    const {job, children} = flow;
+    const {job, children = []} = flow;
     const filteredChildren = children.filter((child) => child);
     const queueName = job.queueName;
 
-    if (filteredChildren && filteredChildren.length > 0) {
+    if (filteredChildren.length > 0) {
       return {
         job: {...job, queueName},
         children: filteredChildren.map((child) => this.processFlow(child)),
